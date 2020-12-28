80ba3c282d6f49372f625fb83c8206dea56e7f74
llomellamomario <llomellamomario@gmail.com>
2020-12-28 11:17:47 +0100

    - Finished basic game  - Changed text-based screen drawing with curses based drawing  - Refactored to a single class  -  Initial prep for testing

---
+++ app/game_of_life.py
@@ -0,0 +1,83 @@
+import copy
+import random
+import curses, _curses
+
+
+class game_of_life():
+
+    def __init__(self, x_size=None, y_size=None, file = None):
+        if x_size is not None and y_size is not None and file is None:
+            self.gof_board = self.random_state(x_size, y_size)
+            self.x_size = x_size
+            self.y_size = y_size
+        elif x_size is None and y_size is None and file is not None:
+            pass
+        else:
+            raise RuntimeError('Please provide either a size or a premade petri dish')
+        self.screen: _curses.window = curses.initscr()
+        self.window: _curses.window = curses.newwin(self.x_size + 2, self.y_size + 2, 0, 0)  # curses doesn't like for us to
+        # write to the lower right hand corner of a window, so 2 offset: one for the border, 1 to avoid the corner
+
+    def random_state(self, x: int, y: int):
+        return [[random.randrange(0, 2) for j in range(y)] for i in range(x)]
+
+    def render(self):
+        conversion_dict = {0: ' ', 1: '#'}
+        print('+' + '-' * len(self.gof_board) + '+')
+        for i, row in enumerate(self.gof_board):
+            column = '|'
+            for space in row:
+                column += conversion_dict[space]
+            column += '|'
+            print(column)
+        print('+' + '-' * len(self.gof_board) + "+")
+
+    def render_curses(self):
+        conversion_dict = {0: ' ', 1: curses.ACS_CKBOARD}
+        self.window.clear()
+        self.window.box()
+        curses.curs_set(0)
+        for i, row in enumerate(self.gof_board):
+            for j, space in enumerate(row):
+                self.window.addch(i + 1, j + 1, conversion_dict[space])  # offset position by one to clear the border
+        self.window.refresh()
+
+    def next_board_state(self):
+        updated_board = copy.deepcopy(self.gof_board)
+        for i, row in enumerate(self.gof_board):
+            for j, point in enumerate(row):
+                alive = 0
+                for k in range(i - 1, i + 2):
+                    for m in range(j - 1, j + 2):
+                        if k < 0 or m < 0:
+                            pass
+                        elif k >= len(self.gof_board) or m >= len(self.gof_board[0]):
+                            pass
+                        else:
+                            alive += self.gof_board[k][m]
+                if self.gof_board[i][j] == 0:
+                    if alive == 3:
+                        updated_board[i][j] = 1
+                    else:
+                        pass
+                else:
+                    alive -= 1  # Remove the center square
+                    if alive <= 1 or alive > 3:
+                        updated_board[i][j] = 0
+                    else:
+                        updated_board[i][j] = 1
+        self.gof_board = updated_board
+
+
+def main():
+    board_size_x, board_size_y = 50, 150
+    gof_board = game_of_life(x_size=board_size_x, y_size=board_size_y)
+    for i in range(1000):
+        gof_board.render_curses()
+        gof_board.next_board_state()
+        curses.napms(10)
+    # next_board_state(gof_board)
+
+
+if __name__ == '__main__':
+    main()
